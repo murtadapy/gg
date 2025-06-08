bfdfd3a5f42d2aeb22f5f672afc0c74a67d7f054
Murtada <murtada.altarouti@gmail.com>
2025-06-08 21:41:51 +0300

    Created init command

--- gg/commands/initiate.py
+++ gg/commands/initiate.py
@@ -1,11 +1,33 @@
+import sys
+import os
+import ctypes
+
 from gg.base import CommandBase
+from gg.database import Database
 from gg.logger import Logger
 
 
 class InitiateCommand(CommandBase):
     def __init__(self, logger: Logger) -> None:
         super().__init__(logger=logger)
+        self.path = os.path.join(sys.path[0], ".gg")
+
+    def _create_database(self) -> None:
+        Database.create_database(self.path)
+
+    def _create_repostiory(self) -> None:
+        os.makedirs(self.path, exist_ok=True)
+
+        if sys.platform == "win32":
+            ctypes.windll.kernel32.SetFileAttributesW(self.path, 0x02)
 
     def execute(self) -> None:
         self.logger.pulse("Executing initate command")
-        ...
+
+        self._create_repostiory()
+        self.logger.pulse("Created Repository folder")
+
+        self._create_database()
+        self.logger.pulse("Created database")
+
+        self.logger.info(f"Initialized empty gg repository in {self.path}")
---
+++ gg/database.py
@@ -0,0 +1,66 @@
+import os
+import sqlite3
+
+
+DATABASE = "gg.db"
+
+
+TEMPLATE = """
+    PRAGMA foreign_keys = ON;
+
+    CREATE TABLE IF NOT EXISTS blobs (
+        id TEXT PRIMARY KEY,
+        content BLOB NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS commits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        uniqueId TEXT NOT NULL,
+        authorEmail TEXT NOT NULL,
+        authorName TEXT NOT NULL,
+        date TEXT NOT NULL,
+        parentCommitId INTEGER,
+        FOREIGN KEY(parentCommitId) REFERENCES commits(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS commit_blobs (
+        commitId INTEGER NOT NULL,
+        blobId TEXT NOT NULL,
+        path TEXT NOT NULL,
+        PRIMARY KEY (commitId, path),
+        FOREIGN KEY(commitId) REFERENCES commits(id),
+        FOREIGN KEY(blobId) REFERENCES blobs(id)
+    ) WITHOUT ROWID;
+
+    CREATE TABLE IF NOT EXISTS sprints (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        baseCommitId INTEGER,
+        FOREIGN KEY(baseCommitId) REFERENCES commits(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS sprint_blobs (
+        sprintId INTEGER NOT NULL,
+        blobId TEXT NOT NULL,
+        path TEXT NOT NULL,
+        PRIMARY KEY (sprintId, path),
+        FOREIGN KEY(sprintId) REFERENCES sprints(id),
+        FOREIGN KEY(blobId) REFERENCES blobs(id)
+    ) WITHOUT ROWID;
+"""
+
+
+class Database:
+    @staticmethod
+    def create_database(path: str) -> None:
+        path = os.path.join(path, DATABASE)
+        with sqlite3.connect(path) as connection:
+            connection.executescript(TEMPLATE)
+            connection.commit()
+
+    @staticmethod
+    def get_connection(path: str) -> sqlite3.Connection:
+        path = os.path.join(path, DATABASE)
+        connection = sqlite3.connect(path)
+        connection.execute("PRAGMA foreign_keys = ON")
+        return connection
