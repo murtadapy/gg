527a9caf1be7dab92a4fc200f23dbbb62b40d303
Murtada <murtada.altarouti@gmail.com>
2025-06-08 12:31:47 +0300

    Built the structure of main module

--- gg/__main__.py
+++ gg/__main__.py
@@ -1,5 +1,4 @@
 from gg import gg
 
-
 if __name__ == "__main__":
     gg.run()
--- gg/gg.py
+++ gg/gg.py
@@ -3,8 +3,13 @@ import argparse
 
 def parse() -> None:
     parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    subparsers.add_parser('init', help="Initiate gg repostiroy")
+
     parser.parse_args()
 
 
 def run() -> None:
-    ...
+    parse()
