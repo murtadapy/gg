97892f8d69fd848c61142502aef88e9f8dc4c139
Murtada <murtada.altarouti@gmail.com>
2025-06-09 17:00:58 +0300

    Fixed sql file & created create sprint query

--- gg/database.py
+++ gg/database.py
@@ -8,8 +8,20 @@ class Database:
         self.path = path
         self.database = os.path.join(path, "gg.db")
 
+    def _get_connection(self) -> sqlite3.Connection:
+        return sqlite3.connect(self.database)
+
     def create_database(self) -> None:
-        with sqlite3.connect(self.database) as connection:
+        with self._get_connection() as connection:
             with resources.open_text("gg", "database.sql") as sql:
                 connection.executescript(sql.read())
+            connection.commit()
+
+    def create_sprint(self,
+                      sprint_name: str,
+                      base_commit_id: int | None) -> None:
+        with self._get_connection() as connection:
+            connection.execute("""
+                               INSERT INTO SPRINT(NAME, BASE_COMMIT_ID)
+                               VALUES (?, ?)""", (sprint_name, base_commit_id))
             connection.commit()
