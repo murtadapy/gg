ed9b8440ce0282bd78687de61d69a98a310faefa
Murtada <murtada.altarouti@gmail.com>
2025-06-10 23:37:36 +0300

    Fixed database structuree

--- gg/commands/status.py
+++ gg/commands/status.py
@@ -17,3 +17,17 @@ class StatusCommand(CommandBase):
 
     def execute(self) -> None:
         self.logger.pulse("Executing status command")
+
+        self.logger.pulse("Checking if repository exist")
+        if not self.file_manager.check_if_repository_exists():
+            self.logger.info("This is not a gg repository")
+            return
+
+        # Get all blobs of current sprint by doing the following:
+            # Get all blobs in the tree of the repository
+            # Get current sprint last commit id
+            # Find all blobs of the last commit id
+            # If we didn't find all blobs init,we search on its children
+            # recursively
+            # once we get each blob, then we compare its SHA-256 in database
+            # And check if it is the same or not.
