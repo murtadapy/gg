146bee05b58ab5e1cb86254569459b8fd980d28d
Murtada <murtada.altarouti@gmail.com>
2025-06-10 18:31:48 +0300

    Added config table

--- gg/commands/initiate.py
+++ gg/commands/initiate.py
@@ -11,14 +11,17 @@ class InitiateCommand(CommandBase):
     def __init__(self, path: str, database: Database, logger: Logger) -> None:
         super().__init__(path=path, database=database, logger=logger)
 
-    def _create_database(self) -> None:
-        self.database.create_database()
+    def _check_if_exist(self) -> bool:
+        return os.path.exists(self.path)
 
     def _create_repostiory(self) -> None:
-        os.makedirs(self.path, exist_ok=True)
+        os.makedirs(self.path)
 
         if sys.platform == "win32":
             ctypes.windll.kernel32.SetFileAttributesW(self.path, 0x02)
+
+    def _create_database(self) -> None:
+        self.database.create_database()
 
     def _create_main_sprint(self) -> None:
         self.database.create_sprint(sprint_name="main")
@@ -26,13 +29,21 @@ class InitiateCommand(CommandBase):
     def execute(self) -> None:
         self.logger.pulse("Executing initate command")
 
+        self.logger.pulse("Checking if repository exists")
+        if self._check_if_exist():
+            self.logger.info(f"The repository is already exist in {self.path}")
+            return
+
+        self.logger.pulse("Creating repository folder")
         self._create_repostiory()
-        self.logger.pulse("Created Repository folder")
+        self.logger.pulse("Created repository folder successfully")
 
+        self.logger.pulse("Creating the repository database")
         self._create_database()
-        self.logger.pulse("Created database")
+        self.logger.pulse("Created database successfully")
 
+        self.logger.pulse("Creating the main sprint")
         self._create_main_sprint()
-        self.logger.pulse("Created main sprint")
+        self.logger.pulse("Created main sprint successfully")
 
         self.logger.info(f"Initialized empty gg repository in {self.path}")
